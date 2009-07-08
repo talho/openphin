257c366ee3021848ba300a7c06771babc6c11a06
Keith Gaddis <keith.gaddis@karmajunkie.com>
2009-07-08 11:39:44 -0500

    RoleRequest bug fix (not assigning to self properties) and validations added (requester, role cannot be blank

--- app/models/role_request.rb
+++ app/models/role_request.rb
@@ -1,20 +1,23 @@
 class RoleRequest < ActiveRecord::Base
+  validates_presence_of :role_id
+  validates_presence_of :requester_id
+  
   def requester
     _requester||=PhinPerson.find(:first, :attribute => "externalUID", :value => attributes["requester_id"]) unless attributes["requester_id"].nil?
   end
   def requester=(val)
-    approver_id=val.id unless val.nil? || val.id.nil?
+    self[:requester_id]=val.id unless val.nil? || val.id.nil?
   end
   def approver
     _approver||=PhinPerson.find(:first, :attribute => "externalUID", :value => attributes["approver_id"] ) unless attributes["approver_id"].nil?
   end
   def approver=(val)
-    approver_id=val.id unless val.nil? || val.id.nil?
+    self[:approver_id]=val.id unless val.nil? || val.id.nil?
   end
   def role
     _role||=PhinPerson.find(:first, :attribute => "externalUID", :value => attributes["role_id"] ) unless attributes["role_id"].nil?
   end
   def role=(val)
-    role_id=val.id unless val.nil? || val.id.nil?
+    self[:role_id]=val.id unless val.nil? || val.id.nil?
   end
 end
