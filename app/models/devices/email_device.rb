e2c9646bbbb68431d77796f0a79d767b9c624106
Brandon Keepers <brandon@collectiveidea.com>
2009-07-14 16:36:27 -0400

    Annotated models

--- app/models/devices/email_device.rb
+++ app/models/devices/email_device.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: devices
+#
+#  id            :integer         not null, primary key
+#  user_id       :integer
+#  type          :string(255)
+#  description   :string(255)
+#  name          :string(255)
+#  coverage      :string(255)
+#  emergency_use :boolean
+#  home_use      :boolean
+#
+
 class Devices::EmailDevice < Device
   
   serialize :options
--- spec/models/alert_spec.rb
+++ spec/models/alert_spec.rb
@@ -5,7 +5,7 @@
 #  id          :integer         not null, primary key
 #  title       :string(255)
 #  message     :text
-#  severety    :string(255)
+#  severity    :string(255)
 #  status      :string(255)
 #  acknowledge :boolean
 #  author_id   :integer
