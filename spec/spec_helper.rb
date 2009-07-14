0b4ed6046131ce1f54676ed8eae467524a2af9ab
Zach Dennis <zach.dennis@gmail.com>
2009-07-14 16:38:15 -0400

    Implemented scenario: Signing up as a public role without required fields should display errors * extracted model macros for ensuring fields are accessible, #should_make_fielsd_accessible

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,9 +5,6 @@ require File.dirname(__FILE__) + "/../config/environment" unless defined?(RAILS_
 require 'spec/autorun'
 require 'spec/rails'
 
-# Requires supporting files with custom matchers and macros, etc,
-# in ./support/ and its subdirectories.
-Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
 Dir[File.dirname(__FILE__) + '/macros/*.rb'].each{ |f| require f }
 
 Spec::Runner.configure do |config|
