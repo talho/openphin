6dbfc8712ec60d412d4be832963e3399186d0583
Brandon Keepers <brandon@collectiveidea.com>
2009-07-15 09:46:58 -0400

    updated rspec files

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,6 +9,9 @@ require 'spec/rails'
 # in ./support/ and its subdirectories.
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
 Dir[File.dirname(__FILE__) + '/macros/*.rb'].each{ |f| require f }
+# Requires supporting files with custom matchers and macros, etc,
+# in ./support/ and its subdirectories.
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
 
 Spec::Runner.configure do |config|
   # If you're not using ActiveRecord you should remove these
