15adc61afbc414e4bfc31f884bfbdf996d4e6429
Daniel Morrison <daniel@collectiveidea.com>
2009-07-14 23:28:08 -0400

    Added user seed data

---
+++ db/fixtures/users.rb
@@ -0,0 +1,47 @@
+User.seed(:first_name, :last_name) do |m|
+  m.first_name = 'Keith'
+  m.last_name = 'Gaddis'
+  m.email = 'keith@example.com'
+  m.password = 'password'
+  m.password_confirmation = 'password'
+end
+
+User.seed(:first_name, :last_name) do |m|
+  m.first_name = 'Ethan'
+  m.last_name = 'Waldo'
+  m.email = 'ethan@example.com'
+  m.password = 'password'
+  m.password_confirmation = 'password'
+end
+
+User.seed(:first_name, :last_name) do |m|
+  m.first_name = 'Jason'
+  m.last_name = 'Phipps'
+  m.email = 'jason@example.com'
+  m.password = 'password'
+  m.password_confirmation = 'password'
+end
+
+User.seed(:first_name, :last_name) do |m|
+  m.first_name = 'Brandon'
+  m.last_name = 'Keepers'
+  m.email = 'brandon@example.com'
+  m.password = 'password'
+  m.password_confirmation = 'password'
+end
+
+User.seed(:first_name, :last_name) do |m|
+  m.first_name = 'Daniel'
+  m.last_name = 'Morrison'
+  m.email = 'daniel@example.com'
+  m.password = 'password'
+  m.password_confirmation = 'password'
+end
+
+User.seed(:first_name, :last_name) do |m|
+  m.first_name = 'Zach'
+  m.last_name = 'Dennis'
+  m.email = 'zach@example.com'
+  m.password = 'password'
+  m.password_confirmation = 'password'
+end
