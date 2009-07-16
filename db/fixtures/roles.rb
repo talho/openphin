ef657aadba9e84ddaf624336d76fc950092fbb41
Keith Gaddis <keith.gaddis@karmajunkie.com>
2009-07-16 08:48:15 -0500

    added the 35 public health roles in roles.csv for seed-fu

--- db/fixtures/roles.rb
+++ db/fixtures/roles.rb
@@ -1,11 +1,10 @@
-Role.seed(:name) do |r|
-  r.name = 'Health Alert & Communications Coordinator'
-end
+require 'fastercsv'
 
-Role.seed(:name) do |r|
-  r.name = 'Epidemiologist'
-end
-
-Role.seed(:name) do |r|
-  r.name = 'Health Officer'
+FasterCSV.open(File.dirname(__FILE__) + '/roles.csv', :headers => true) do |roles|
+  roles.each do |row|
+    Role.seed(:name, :approval_required) do |role|
+      role.name = row['role']
+      role.approval_required = row['approval_required'].downcase == 'true'
+    end
+  end
 end
