2aa174fb043d1f574a10d4e9c2b69550bdab171f
Zach Dennis <zach.dennis@gmail.com>
2009-07-16 15:17:16 -0400

    Added DEFAULT_HOST constant in environment.rb and updated relative paths used by mailers to be full-blown URLs.

--- app/mailers/signup_mailer.rb
+++ app/mailers/signup_mailer.rb
@@ -3,7 +3,7 @@ class SignupMailer < ActionMailer::Base
   def confirmation(user)
     recipients user.email
     subject "Confirm your email"
-    body :confirm_link => user_confirmation_path(user, user.token)
+    body :confirm_link => user_confirmation_url(user, user.token)
   end
   
   def admin_notification_of_role_request(role_request, admin)
