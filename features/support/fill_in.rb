35d883d880b8f7890a3f4fe2d771c472a8437723
Zach Dennis <zach.dennis@gmail.com>
2009-07-16 09:51:18 -0400

    Moved #fill_in_signup_form into features/support/fill_in.rb

---
+++ features/support/fill_in.rb
@@ -0,0 +1,37 @@
+module FeatureHelpers
+  module FillInMethods
+    def fill_in_signup_form(table = nil)
+      fields={"Email"=> "john@example.com",
+          "Password"=> "password",
+          "Password confirmation"=> "password",
+          "First name"=> "John",
+          "Last name"=> "Smith",
+          "Preferred name"=> "Jonathan Smith",
+          "Are you with any of these organizations"=> "Red Cross",
+          "What County"=> "Dallas County",
+          "What is your role within the health department"=> "Health Alert and Communications Coordinator",
+          "Preferred language"=> "English"
+      }
+      if table.is_a?(Hash)
+        fields.merge!(table)
+      elsif !table.nil?
+        fields.merge!(table.rows_hash)
+      end
+      fields.each do |field, value|
+        value = "" if value == "<blank>"
+        case field
+        when 'Email', 'Password', 'Password confirmation', 'First name', 'Last name', 'Preferred name'
+          fill_in field, :with => value
+        when 'What County', 'Preferred language', 
+          'What is your role within the health department', 
+          'Are you with any of these organizations'
+            select value, :from => field
+        else
+          raise "Unknown field: #{field}: Please update this step if you intended to use this field."
+        end
+      end
+    end
+  end
+end
+
+World(FeatureHelpers::FillInMethods)
