fa70cbb0464edb8a01cbf41bc97999f2e4dbce23
Daniel Morrison <daniel@collectiveidea.com>
2009-07-17 12:17:06 -0400

    Added seed data for organization types

---
+++ db/fixtures/organization_types.rb
@@ -0,0 +1,11 @@
+OrganizationType.seed(:name) do |o|
+  o.name = 'Local Health Department'
+end
+
+OrganizationType.seed(:name) do |o|
+  o.name = 'State Health Department'
+end
+
+OrganizationType.seed(:name) do |o|
+  o.name = 'Non-profit'
+end
