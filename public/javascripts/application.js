0c9002e1aad82c07f4bbf6cd9ade1dee2df991c7
Brandon Keepers <brandon@collectiveidea.com>
2009-07-17 10:45:06 -0400

    Hide health professional fields if checkbox not selected

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -14,10 +14,20 @@
       request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
     }
   });
+  
+  $.fn.toggleCheckbox = function(target) {
+    return this.each(function() {
+      $(target)[this.checked ? 'show' : 'hide']();
+    });
+  };
 })(jQuery);
 
 
-$(function() {  
+jQuery(function($) {
+  $('#health_professional').toggleCheckbox('#health_professional_fields').click(function() {
+    $(this).toggleCheckbox('#health_professional_fields');
+  });
+  
   $('select.crossSelect[multiple="multiple"]').crossSelect({clickSelects: true});
   
   $("#alert_user_ids").fcbkcomplete({
@@ -31,38 +41,3 @@ $(function() {
   });
   
 });
-
-// function jsAddRole()
-// {
-//     var rselect=$("#phin_roles_select")[0];
-//     var jselect=$("#phin_jurisdictions_select")[0];
-//     var list=$("#phin_role_list ul")[0];
-//     var idx=role_ct++;
-//     $(list).append(
-// 
-//         "<li><input type='hidden' name='phin_roles["+(idx)+"][role_id]' value='"+
-//             $(rselect.options[rselect.selectedIndex]).val()+"'/>"+
-//             "<input type='hidden' name='phin_roles["+(idx)+"][jurisdiction_id]' value='"+
-//             $(jselect.options[jselect.selectedIndex]).val()+"'/>"+
-//             $(rselect.options[rselect.selectedIndex]).text()+" (" +
-//             $(jselect.options[jselect.selectedIndex]).text()+ ")</li>");
-//     
-// }
-// 
-// $(document).ready(function() {  
-//   $("input#phin_person_first_name").autocomplete({
-//     ajax: "auto_complete_for_phin_person_first_name" 
-//   })
-// });
-// 
-// function remove_role(id)
-// {
-//   if(id+"" == "undefined") return;
-//   
-//   var list=$("#phin_role_list ul")[0];
-//   var rselect=$("#phin_roles_"+id)[0];
-//   $(rselect).append(
-//     "<input type='hidden' name='phin_roles["+(id)+"][_delete]' value='true'/>"
-//   );
-//   $(rselect).slideUp(1000);
-// }
