0310d52d4669f200a6afc1a0fc2128c01c6a734e
Ethan Waldo <ewaldo@quickdev.healthetechs.com>
2009-07-20 09:30:05 -0500

    Forgot to add migration on last commit

---
+++ db/migrate/20090718162242_add_alert_acknowledgement_to_alerts.rb
@@ -0,0 +1,11 @@
+class AddAlertAcknowledgementToAlerts < ActiveRecord::Migration
+  def self.up
+    add_column :alerts, :alert_acknowledged, :boolean
+    add_column :alerts, :alert_acknowledged_timestamp, :date_time
+  end
+
+  def self.down
+    remove_column :alerts, :alert_acknowledged_timestamp
+    remove_column :alerts, :alert_acknowledged
+  end
+end
