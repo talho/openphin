95ff46f6ed36a1225fd393c4bd22874a4b35bfa1
Daniel Morrison <daniel@collectiveidea.com>
2009-07-27 12:26:21 -0500

    Added exception_notification plugin as a submodule

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@
 class ApplicationController < ActionController::Base
   include CachingPresenter::InstantiationMethods
   include Clearance::Authentication
+  include ExceptionNotifiable
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
