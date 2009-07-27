23e0020e1d12b7c2a6718337d74a5a59a2ced175
Zach Dennis <zach.dennis@gmail.com>
2009-07-27 10:54:16 -0500

    added some labeled input JavaScript to default password fields to six asterisks, which are NOT submitted when the form is submitted

---
+++ public/javascripts/labeled-input.js
@@ -0,0 +1,24 @@
+// Mark VanHolstyn, Mutually Human Software
+$(document).ready(function() {
+  var hideLabel = function() {
+    if($(this).attr("value") == $(this).attr("data-label")) {
+      $(this).attr("value", "");
+      $(this).removeClass("label");
+    }
+  };
+  
+  var showLabel = function() {
+    if($(this).attr("value") == "") { 
+      $(this).attr("value", $(this).attr("data-label"));
+      $(this).addClass("label");
+    }
+  };
+  
+  $("form").livequery("submit", function() {
+    $(this).select('input[data-label], textarea[data-label]').each(hideLabel);
+  });
+  $('input[data-label], textarea[data-label]').livequery("focus", hideLabel);
+  $('input[data-label], textarea[data-label]').livequery("blur", showLabel);
+
+  $('input[data-label], textarea[data-label]').each(showLabel);
+});
