eed95dea5601fd94b2a66dcfc5c7ef30b11e593c
Daniel Morrison <daniel@collectiveidea.com>
2009-07-30 14:55:21 -0500

    Added delayed_job to deploy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -47,7 +47,7 @@ private
   def assign_public_role_if_no_role_is_provided
     role_requests = params[:user][:role_requests_attributes]
     if role_requests.has_key?("0") && role_requests["0"]["role_id"].blank?
-       role_requests["0"]["role_id"] = Role.find_by_name("Public").id
+       role_requests["0"]["role_id"] = Role.public.id
     end
   end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,29 +1,5 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe UsersController, "as a logged in user" do
-  before(:each) do
-    login_as_user
-  end
   
-  describe 'search' do
-    before do
-      @user1 = Factory(:user, :last_name => 'Smith')
-      @user2 = Factory(:user, :last_name => 'Smithers')
-    end
-    
-    def do_action
-      get :search, :q => 'smith'
-    end
-      
-    it 'should search the users' do
-      User.should_receive(:search).with('smith').and_return([])
-      do_action
-    end
-    
-    it 'should return users as a json object' do
-      do_action
-      response.body.should have_text(Regexp.new(%|"caption": "#{@user1.name}"|))
-      response.body.should have_text(Regexp.new(%|"value": #{@user1.id}|))
-    end
-  end
 end
