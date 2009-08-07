f316393a4f8ab7bd29ddd0788ff6e49c56f79d7f
Zach Dennis <zach.dennis@gmail.com>
2009-08-07 14:04:32 -0400

    Removed region model and updated seed data to load regions as children of texas and counties as children of their region.

--- app/models/region.rb
+++
@@ -1,3 +0,0 @@
-class Region < ActiveRecord::Base
-  validates_presence_of :name
-end
--- db/fixtures/jurisdictions.rb
+++ db/fixtures/jurisdictions.rb
@@ -10,19 +10,18 @@ texas.move_to_child_of(federal)
 
 FasterCSV.open(File.join(RAILS_ROOT, "db/fixtures/regions.csv"), :headers => true) do |roles|
   roles.each do |row|
-    Region.seed(:name) do |region|
-      region.id = row['id']
+    Jurisdiction.seed(:name) do |region|
       region.name = row['name']
-    end
+    end.move_to_child_of(texas)
   end
 end
 
 FasterCSV.open(File.join(RAILS_ROOT, "db/fixtures/jurisdictions.csv"), :headers => true) do |roles|
   roles.each do |row|
-    Jurisdiction.seed(:name) do |region|
+    m = Jurisdiction.seed(:name) do |region|
       region.name = row['name'].strip
-      region.region_id = row['region_id']
       region.fips_code = row['fips_code']
-    end.move_to_child_of(texas)
+    end
+    m.move_to_child_of Jurisdiction.find_by_name!(row['region_name'])
   end
 end
