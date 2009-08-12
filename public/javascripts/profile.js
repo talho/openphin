8be6abe68c17888e8663e07fd905d6a90dd2856c
Steve Smith <steve@steve-smiths-macbook.local>
2009-08-12 08:45:25 -0400

    Profile edit is now styled.

--- public/javascripts/profile.js
+++ public/javascripts/profile.js
@@ -1,8 +1,15 @@
 (function($) {
-  $('.edit_user_profile .add_device').live('click', function(e) {
-    $('.edit_user_profile #new_device').show();
+  $('form a.add_device').live('click', function() {
+    $('.new_device').show();
     $(this).hide();
     return false;
+  });
+  
+  $(document).ready(function() {
+    $('.edit_user #device_type').change(function() {
+      $(this).closest('div.aside').find('p.device').hide();
+      $('p.' + $(this).val()).show();
+    });
   });
   
   $(function() {
