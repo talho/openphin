f16785362ab8dd27ec05c64657de4ab52283d106
Ethan Waldo <ewaldo@quickdev.healthetechs.com>
2009-09-18 11:45:13 -0500

    Changed to new production domain name

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@ RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 require File.join(File.dirname(__FILE__), 'initializers', 'utilities')
 
-HOST = "txphin.texashan.org"
+HOST = "www.txphin.org"
 
 Rails::Initializer.run do |config|
   # Settings in config/environments/* take precedence over those specified here.
