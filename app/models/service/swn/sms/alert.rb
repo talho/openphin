a14447618462d46cb94af8a377e75f2f70415313
Ethan Waldo <ewaldo@quickdev.healthetechs.com>
2009-09-30 16:31:32 -0500

    Added caller id to SWN devices.  Blackberry doesn't seem to use it for the from-header though.

--- app/models/service/swn/sms/alert.rb
+++ app/models/service/swn/sms/alert.rb
@@ -65,6 +65,7 @@ class Service::SWN::SMS::Alert < Service::SWN::SMS::Base
       xml.swn(:introName, alert.author.display_name)
       xml.swn(:introOrganization, alert.from_organization) if !alert.from_organization.blank?
       xml.swn(:introOrganization, alert.from_jurisdiction) if !alert.from_jurisdiction.blank?
+      xml.swn(:phone, alert.caller_id) if !alert.caller_id.blank?
     end
   end
 
