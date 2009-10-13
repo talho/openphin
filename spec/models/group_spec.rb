0280a2573916c0daffaaaacdf33618f34a905bea
Ethan Waldo <ewaldo@quickdev.healthetechs.com>
2009-10-13 14:42:41 -0500

    Forgot to check specs...again

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -2,8 +2,10 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Group do
   before(:each) do
+    jurisdiction = Factory(:jurisdiction)
+    Factory(:jurisdiction).move_to_child_of(jurisdiction)
     @valid_attributes = {
-      
+      :owner_id => Factory(:user).id
     }
   end
 
