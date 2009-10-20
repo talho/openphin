0e7007b54b28f6d2621c9831b9645d8ee1d82a62
Brandon Keepers <brandon@collectiveidea.com>
2009-10-20 12:59:08 -0400

    Fix device_type to use "Service" namespace

--- spec/models/alert_device_type_spec.rb
+++ spec/models/alert_device_type_spec.rb
@@ -15,7 +15,7 @@ describe AlertDeviceType do
   
   describe "device_type" do
     it "should constantize class for 'Phone'" do
-      AlertDeviceType.new(:device => 'Service::Phone').device_type.should == Service::Phone
+      AlertDeviceType.new(:device => 'Phone').device_type.should == Service::Phone
     end
     
     it "should not do evil things" do
