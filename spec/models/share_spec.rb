57d330e2b6de8f9ea69778bfdb23acdb40045995
Daniel Morrison <daniel@collectiveidea.com>
2009-10-21 21:48:02 +0100

    Fixing a spec, need to do the full require

--- spec/models/share_spec.rb
+++ spec/models/share_spec.rb
@@ -10,18 +10,8 @@
 #  updated_at  :datetime
 #
 
-require 'spec_helper'
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Share do
-  before(:each) do
-    @valid_attributes = {
-      :document_id => 1,
-      :user_id => 1,
-      :folder_id => 1
-    }
-  end
 
-  it "should create a new instance given valid attributes" do
-    Share.create!(@valid_attributes)
-  end
 end
