dc3d3e20a8eb6d9759b021a2e7e1ecaa9da2fa4e
Daniel Morrison <daniel@collectiveidea.com>
2009-10-22 16:17:15 +0100

    Limited document sharing & receiving to only users with a non-public role.

--- app/mailers/document_mailer.rb
+++ app/mailers/document_mailer.rb
@@ -1,7 +1,7 @@
 class DocumentMailer < ActionMailer::Base
   
-  def document(document, target)
-    bcc target.audience.recipients.map(&:formatted_email)
+  def document(document, target, recipients)
+    bcc recipients.map(&:formatted_email)
     from DO_NOT_REPLY
     subject "#{target.creator.name} shared a document with you"
     body :document => document
