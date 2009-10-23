0ae05c6f246d0974e1a1f8dc3088603daf8827d1
Daniel Morrison <daniel@collectiveidea.com>
2009-10-23 17:09:04 +0100

    Revert "Resetting the db after everything seems to fix the issues, but should be a better way."
    This reverts commit dfaee3616edb81c8f17c04acb0ce107f6417a2a7.

--- features/support/env.rb
+++ features/support/env.rb
@@ -75,9 +75,5 @@ Spork.each_run do
     # load application-wide fixtures
     Dir[File.join(RAILS_ROOT, "features/fixtures", '*.rb')].sort.each { |fixture| load fixture }
   end
-  
-  After do 
-    reset_database!
-  end
 
 end
