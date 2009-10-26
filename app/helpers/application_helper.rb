c4935b5062174205d7892b8443dbbaf754e1fba5
Rich Boldway <rich@boldway.com>
2009-10-25 22:35:33 -0500

    create and used the new tagged_form_for form helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,12 @@ module ApplicationHelper
   def d(str,options=nil)
     content_tag :div, str, options
   end
+  
+  def tagged_form_for(name, *args, &block)
+    options = args.last.is_a?(Hash) ? args.pop : {}
+    options = options.merge(:builder => TaggedBuilder)
+    args = (args << options)
+    form_for(name, *args, &block)
+  end
+  
 end
