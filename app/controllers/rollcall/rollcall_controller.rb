745113017e83daf00a9e85ed70033c9796cdde00
Keith Gaddis <keith.gaddis@karmajunkie.com>
2009-10-26 15:57:11 -0500

    Fixed: y-axis range on graphs is now dynamic.  Also added a fork of googlecharts as a plugin submodule.

--- app/controllers/rollcall/rollcall_controller.rb
+++ app/controllers/rollcall/rollcall_controller.rb
@@ -51,12 +51,9 @@ class Rollcall::RollcallController < ApplicationController
                          :title => "Average % Absenteeism",
                          :axis_with_labels => "x,y",
                          :axis_labels => xlabels,
-                         #                         :max => 30,
                          :legend => @districts.map(&:name),
                          :data => @districts.map{|d| d.recent_absentee_rates(timespan).map{|m|m*100}},
-                         #                         :custom => "chxr=1,0,30",
                          :encoding => "text"
-#                         :max_value => 30
       )
       ethans_crazy_absenteeism_summary_code
     end
