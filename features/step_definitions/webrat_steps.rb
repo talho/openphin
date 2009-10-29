7e598e24f7a6b87d02f875f4c76c35d372f0ae52
Daniel Morrison <daniel@collectiveidea.com>
2009-10-29 22:15:03 +0000

    Fixing some webrat steps I clobbered

--- features/step_definitions/webrat_steps.rb
+++ features/step_definitions/webrat_steps.rb
@@ -187,3 +187,20 @@ end
 Then /^show me the page$/ do
   save_and_open_page
 end
+
+Then /^I should see:$/ do |table|
+  table.raw.each do |row|
+  response.should contain(row.join)
+end
+
+Then /^I should be on (.+)$/ do |page_name|
+  URI.parse(current_url).path.should == path_to(page_name)
+end
+
+Then /^I should be redirected to (.+)$/ do |page_name|
+  URI.parse(current_url).path.should == path_to(page_name)
+end
+
+Then /^I have loaded (.*) for "([^\"]*)"$/ do |page, title|
+  URI.parse(current_url).path.should == path_to(page,title)
+end
