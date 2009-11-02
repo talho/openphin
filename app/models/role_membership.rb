272b2eae9308626bf97d4b4b698489ef699d0897
Ethan Waldo <ewaldo@quickdev.healthetechs.com>
2009-11-02 13:29:16 -0600

    Changed several named scopes to use lambdas because they need to be resolved at runtime.

--- app/models/role_membership.rb
+++ app/models/role_membership.rb
@@ -22,13 +22,13 @@ class RoleMembership < ActiveRecord::Base
   validates_presence_of :user_id
   validates_uniqueness_of :role_id, :scope => [ :jurisdiction_id, :user_id ]
   named_scope :user_roles, :include => :role, :conditions => {:roles => {:user_role => true}}
-  named_scope :public_roles, :include => :role, :conditions => {:role_id => Role.public.id}
-  named_scope :not_public_roles, :include => :role, :conditions => ["role_id != ?", Role.public.id]
+  named_scope :public_roles, lambda{{ :include => :role, :conditions => {:role_id => Role.public.id} }}
+  named_scope :not_public_roles, lambda{{ :include => :role, :conditions => ["role_id != ?", Role.public.id] }}
 
   named_scope :all_roles, :include => :role
 
   named_scope :alerter, :joins => :role, :conditions => {:roles => {:alerter => true}}
-  named_scope :recent, :conditions => ["created_at > ?",1.days.ago]
+  named_scope :recent, lambda{{ :conditions => ["created_at > ?",1.days.ago] }}
   
   def self.already_exists?(user, role, jurisdiction)
     return true if RoleMembership.find_by_user_id_and_role_id_and_jurisdiction_id(user.id, role.id, jurisdiction.id)
