406f0db438f3e36e1187e9a18b7a2c305af29e2b
Keith Gaddis <keith.gaddis@karmajunkie.com>
2009-11-02 13:51:34 -0600

    * Fixed graph data in school view * added the school selector to school view * refactored school summary view on rollcall index

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,7 +14,7 @@ Spec::Runner.configure do |config|
   config.use_transactional_fixtures = true
   config.use_instantiated_fixtures  = false
   config.fixture_path = RAILS_ROOT + '/spec/fixtures/'
-  
+
   config.include Webrat::Matchers, :type => [:view]
   config.extend SpecHelpers::ModelMacros, :type => [:model]
   config.extend SpecHelpers::ControllerMacros, :type => [:controller, :integration]
