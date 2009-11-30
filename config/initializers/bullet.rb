edbdd94dd2bfbfabb69df812eeccb787e3c1bb94
Daniel Morrison <daniel@collectiveidea.com>
2009-11-30 17:06:29 +0000

    Refactored bullet config file.

--- config/initializers/bullet.rb
+++ config/initializers/bullet.rb
@@ -1,41 +1,19 @@
-file = RAILS_ROOT + "/config/bullet.yml"
-bullet_enable = false
-bullet_alert_enable = false
-bullet_logger_enable = false
-bullet_console_enable = false
-bullet_growl_enable = false
-bullet_rails_logger_enable = false
-bullet_disable_browser_cache_enable = false
-if File.exists?(file)
-  config = YAML.load(IO.read(file))
-  bullet_enable = config[RAILS_ENV]["enable"] if config[RAILS_ENV].has_key?("enable")
-  bullet_alert_enable = config[RAILS_ENV]["alert_enable"] if config[RAILS_ENV].has_key?("alert_enable")
-  bullet_logger_enable = config[RAILS_ENV]["logger_enable"] if config[RAILS_ENV].has_key?("logger_enable")
-  bullet_console_enable = config[RAILS_ENV]["console_enable"] if config[RAILS_ENV].has_key?("console_enable")
-  bullet_growl_enable = config[RAILS_ENV]["growl_enable"] if config[RAILS_ENV].has_key?("growl_enable")
-  bullet_rails_logger_enable = config[RAILS_ENV]["rails_logger_enable"] if config[RAILS_ENV].has_key?("rails_logger_enable")
-  bullet_disable_browser_cache_enable = config[RAILS_ENV]["disable_browser_cache_enable"] if config[RAILS_ENV].has_key?("disable_browser_cache_enable")
-end
+if File.exists?(file = Rails.root + "/config/bullet.yml")
+  bullet_config = YAML.load(file)[Rails.env].symbolize_keys
 
-if bullet_enable
-  Bullet.enable = bullet_enable
-  Bullet.alert = bullet_alert_enable
-  Bullet.bullet_logger = bullet_logger_enable
-  Bullet.console = bullet_console_enable
-  if bullet_growl_enable
-    file = RAILS_ROOT + "/config/growl.yml"
-    growl_host = "localhost"
-    growl_password = nil
-    enable = false
-    if File.exists?(file)
-      config = YAML.load(IO.read(file))
-      growl_host = config[:host] if config.has_key?(:host)
-      growl_password = config[:password] if config.has_key?(:password)
+  if Bullet.enable = bullet_config[:enable]
+    Bullet.alert = bullet_config[:alert_enable]
+    Bullet.bullet_logger = bullet_config[:logger_enable]
+    Bullet.console = bullet_config[:console_enable]
+    if Bullet.growl = bullet_config[:growl_enable]
+      growl_config = {:host => 'localhost'}
+      if File.exists?(growl = Rails.root + "/config/growl.yml")
+        growl_config.merge! YAML.load(growl).symbolize_keys
+      end
+      Bullet.growl_host = growl_config[:host] if defined? Bullet.growl_host
+      Bullet.growl_password = growl_config[:password]
     end
-    Bullet.growl_host = growl_host if defined? Bullet.growl_host
-    Bullet.growl_password = growl_password
+    Bullet.rails_logger = bullet_config[:rails_logger_enable]
+    Bullet.disable_browser_cache = bullet_config[:disable_browser_cache_enable]
   end
-  Bullet.growl = bullet_growl_enable
-  Bullet.rails_logger = bullet_rails_logger_enable
-  Bullet.disable_browser_cache = bullet_disable_browser_cache_enable
 end
