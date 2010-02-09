1351b4de8bb7a9b8ef662b147d56807e3fad27fa
Rich Boldway <rich@Richs.local>
2010-02-09 15:08:36 -0600

    remove User all update, unneccessary and time expense

--- db/migrate/20091217163201_add_deleted_to_user.rb
+++ db/migrate/20091217163201_add_deleted_to_user.rb
@@ -3,12 +3,6 @@ class AddDeletedToUser < ActiveRecord::Migration
     add_column :users, :deleted_at, :datetime, :default => nil
     add_column :users, :deleted_by, :string, :default => nil
     add_column :users, :deleted_from, :string, :default => nil, :limit => 24 # support ip addr 6    
-    User.reset_column_information
-    User.find(:all).each do |u|
-      u.update_attribute :deleted_at, nil
-      u.update_attribute :deleted_by, nil
-      u.update_attribute :deleted_from, nil
-    end
   end
 
   def self.down
