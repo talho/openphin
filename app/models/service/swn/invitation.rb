8842879efb099e2c47562d30b2c449d1eb4bc186
Ethan Waldo <ewaldo@quickdev.healthetechs.com>
2010-03-30 12:34:57 -0500

    Someone failed badly on a mergegit diff!  We lost delivery for invitation system

--- app/models/service/swn/invitation.rb
+++ app/models/service/swn/invitation.rb
@@ -36,25 +36,6 @@ class Service::SWN::Invitation < Service::SWN::Base
     perform_delivery body
   end
 
-  def deliver
-    SWN_LOGGER.info <<-EOT.gsub(/^\s+/, '')
-      |Building invitation message:
-      |  invitation: #{@invitation.id}
-      |  user_ids: #{@users.map(&:id).inspect}
-      |  config: #{@config.options.inspect}
-    EOT
-
-    body = Service::SWN::Email::Invitation.new(
-      :invitation => @invitation,
-      :users => @users,
-      :username => @config['username'],
-      :password => @config['password'],
-      :retry_duration => @config['retry_duration']
-    ).build!
-
-    perform_delivery body
-  end
-
   def deliver_org_membership_notification
     SWN_LOGGER.info <<-EOT.gsub(/^\s+/, '')
       |Building invitation message:
