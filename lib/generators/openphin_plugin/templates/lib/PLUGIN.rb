decb6c9d65a3228441fa9dcc9e4b10b0ac32b020
cacornell <cacornell@gmail.com>
2010-08-04 08:49:04 -0500

    Had missed a few files in templates.

--- lib/generators/openphin_plugin/templates/lib/PLUGIN.rb
+++ lib/generators/openphin_plugin/templates/lib/PLUGIN.rb
@@ -8,8 +8,9 @@ Dir[File.join(File.dirname(__FILE__), '../vendor/plugins/*/lib')].each do |path|
   $LOAD_PATH << path
 end
 
-# Require the open_flash_chart init.rb
-require File.join(File.dirname(__FILE__), '..', 'vendor', 'plugins', 'open_flash_chart', 'init.rb')
+# Require any submodule dependencies here
+# For example, if this depended on open_flash_chart you would require init.rb as follows:
+#   require File.join(File.dirname(__FILE__), '..', 'vendor', 'plugins', 'open_flash_chart', 'init.rb')
 
 # Register the plugin expansion in the $expansion_list global variable
 $expansion_list = [] unless defined?($expansion_list)
@@ -18,6 +19,10 @@ $expansion_list.push(:PLUGIN_NAME) unless $expansion_list.index(:PLUGIN_NAME)
 # Register any required javascript or stylesheet files with the appropriate
 # rails expansion helper
 ActionView::Helpers::AssetTagHelper.register_javascript_expansion(
-  :PLUGIN_NAME => [ "PLUGIN_NAME/PLUGIN_NAME" ])
+  :PLUGIN_NAME => [
+    # add any necessary javascripts like "PLUGIN_NAME/cool_js_stuff.js"
+  ])
 ActionView::Helpers::AssetTagHelper.register_stylesheet_expansion(
-  :PLUGIN_NAME => [])
+  :PLUGIN_NAME => [
+    # add any necessary stylesheets like "PLUGIN_NAME/cool_css_stuff.css"
+  ])
