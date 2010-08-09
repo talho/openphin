cb6d693432faf757e379b93e58158c5e2609ef14
cacornell <cacornell@gmail.com>
2010-08-09 09:10:42 -0500

    moved some deplay code into lib/

--- config/deploy.rb
+++ config/deploy.rb
@@ -38,79 +38,31 @@ task :staging do
 	role :db,  "192.168.30.97", :primary => true
 end
  
-
-after 'deploy:update_code', 'deploy:symlink_configs'
-after 'deploy:symlink_configs', 'deploy:bundle_install'
+# Setup dependencies
+before 'deploy', 'backgroundrb:stop'
+before 'deploy', 'delayed_job:stop'
+before 'deploy', 'sphinx:start_if_not'
+after 'deploy:update_code', 'app:symlinks'
+after 'app:symlinks', 'app:bundle_install'
 after "deploy", "deploy:cleanup"
-namespace :deploy do
-  # Overriding the built-in task to add our rollback actions
-  task :default, :roles => [:app, :web, :jobs] do
-  #  transaction {
-  #    on_rollback do
-  #      # this rollback will fire if this are any tasks after it fail
-  #      puts "  PERFORMING ROLLBACK"
-  #      find_and_execute_task("backgroundrb:restart")
-  #      find_and_execute_task("delayed_job:restart")
-  #      puts "  END ROLLBACK"
-  #    end
-  #  }
-    update
-    restart
-  end
-
-  desc "we need a database. this helps with that."
-  task :symlink_configs, :roles => [:app, :web, :jobs] do 
-    rails_env = fetch(:rails_env, RAILS_ENV)
-    run "ln -fs #{shared_path}/#{RAILS_ENV}.sqlite3 #{release_path}/db/#{RAILS_ENV}.sqlite3"
-    run "ln -fs #{shared_path}/smtp.rb #{release_path}/config/initializers/smtp.rb"
-    run "ln -fs #{shared_path}/database.yml #{release_path}/config/database.yml"
-    run "ln -fs #{shared_path}/sphinx #{release_path}/db/sphinx"
-    run "ln -fs #{shared_path}/backgroundrb.yml #{release_path}/config/backgroundrb.yml"
-    run "ln -fs #{shared_path}/swn.yml #{release_path}/config/swn.yml"
-    run "ln -fs #{shared_path}/email.yml #{release_path}/config/email.yml"
-    run "ln -fs #{shared_path}/phone.yml #{release_path}/config/phone.yml"
-    run "ln -fs #{shared_path}/system.yml #{release_path}/config/system.yml"
-    run "ln -fs #{shared_path}/phin_ms_queues #{release_path}/tmp/phin_ms_queues"
-    run "ln -fs #{shared_path}/rollcall #{release_path}/tmp/rollcall"
-    run "ln -fs #{shared_path}/sphinx.yml #{release_path}/config/sphinx.yml"
-    run "ln -fs #{shared_path}/testjour.yml #{release_path}/config/testjour.yml"
-    run "ln -fs #{shared_path}/tutorials #{release_path}/public/tutorials"
-    run "ln -fs #{shared_path}/attachments #{release_path}/attachments"
-    # for the rollcall plugin
-    run "ln -fs #{release_path}/vendor/plugins/rollcall/lib/workers #{release_path}/lib/workers/rollcall"
-    run "ln -fs #{release_path}/spec/spec_helper.rb #{release_path}/vendor/plugins/rollcall/spec/spec_helper.rb"
-    run "ln -fs #{release_path}/vendor/plugins/rollcall/public/javascript #{release_path}/public/javascripts/rollcall"
-    run "ln -fs #{release_path}/vendor/plugins/rollcall/public/stylesheets #{release_path}/public/stylesheets/rollcall"
-
-    run "cd #{release_path}/vendor/plugins/rollcall; git submodule update -i"
-
-    run "ln -fs #{shared_path}/vendor/cache #{release_path}/vendor/cache"
-    if rails_env == 'test'|| rails_env == 'development' || rails_env == "cucumber"
-      FileUtils.cp("config/backgroundrb.yml.example", "config/backgroundrb.yml") unless File.exist?("config/backgroundrb.yml")
-      FileUtils.cp("config/system.yml.example", "config/system.yml") unless File.exist?("config/system.yml")
-      #FileUtils.cp("config/phone.yml.example", "config/phone.yml") unless File.exist?("config/phone.yml")
-      #FileUtils.cp("config/swn.yml.example", "config/swn.yml") unless File.exist?("config/swn.yml")
-    end
-  end
-
-  desc "run bundle install for gem dependencies"
-  task :bundle_install, :roles => [:app, :web, :jobs] do 
-    run "cd #{release_path}; bundle install --without=test --without=cucumber --without=tools"
-  end
+after 'deploy', "sphinx:rebuild"
+after 'sphinx:rebuild', 'backgroundrb:restart'
+after 'sphinx:rebuild', 'delayed_job:restart'
 
 namespace :deploy do
   # Overriding the built-in task to add our rollback actions
   task :default, :roles => [:app, :web, :jobs] do
     transaction {
       on_rollback do
+        # this rollback will fire if this are any tasks after it fail
         puts "  PERFORMING ROLLBACK, restarting jobs daemons"
         find_and_execute_task("backgroundrb:restart")
         find_and_execute_task("delayed_job:restart")
         puts "  END ROLLBACK"
       end
       update
-      restart
     }
+    restart
   end
 
   desc "unicorn restart"
