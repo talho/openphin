58ceacdd0345a962e38f3c9780255de15f12a66f
Charles DuBose <cdubose@ubuntu.(none)>
2010-08-10 11:28:44 -0500

    Splitting the strip tags string function into a utility javascript file

--- public/javascripts/dashboard/article3panel.js
+++ public/javascripts/dashboard/article3panel.js
@@ -1,21 +1,5 @@
 Ext.namespace('Talho');
 
-String.prototype.stripTags = function(){
-    if(arguments.length<2) strMod=this.replace(/<\/?(?!\!)[^>]*>/gi, '');
-    else{
-        var IsAllowed=arguments[1];
-        var Specified=eval("["+arguments[1]+"]");
-        if(IsAllowed){
-            var strRegExp='</?(?!(' + Specified.join('|') + '))[^>]*>';
-            strMod=this.replace(new RegExp(strRegExp, 'gi'), '');
-        }else{
-            var strRegExp='</?(' + Specified.join('|') + ')[^>]*>';
-            strMod=this.replace(new RegExp(strRegExp, 'gi'), '');
-        }
-    }
-    return strMod;
-}
-
 Talho.Article3Panel = Ext.extend(Ext.util.Observable, {
     constructor: function()
     {
