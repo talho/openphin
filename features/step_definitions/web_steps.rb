dc2a4a0be3f88e6a6aa45084475ee214c8d56b29
Ethan Waldo <ewaldo@talho.org>
2010-08-10 12:24:33 -0500

    Capybara is installed and working.  Converted sign in feature.  Capybara doesn't have access to the controller like webrat did so had to fix a lot of the clearance steps.

--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -9,8 +9,12 @@ require 'uri'
 require 'cgi'
 require File.expand_path(File.join(File.dirname(__FILE__), "..", "support", "paths"))
 
-# Commonly used webrat steps
-# http://github.com/brynary/webrat
+module WithinHelpers
+  def with_scope(locator)
+    locator ? within(locator) { yield } : yield
+  end
+end
+World(WithinHelpers)
 
 Given /^(?:|I )am on (.+)$/ do |page_name|
   visit path_to(page_name)
@@ -20,24 +24,28 @@ When /^(?:|I )go to (.+)$/ do |page_name|
   visit path_to(page_name)
 end
 
-When /^(?:|I )press "([^"]*)"$/ do |button|
-  click_button(button)
+When /^(?:|I )press "([^"]*)"(?: within "([^"]*)")?$/ do |button, selector|
+  with_scope(selector) do
+    click_button(button)
+  end
 end
 
-When /^(?:|I )follow "([^"]*)"$/ do |link|
-  click_link(link)
+When /^(?:|I )follow "([^"]*)"(?: within "([^"]*)")?$/ do |link, selector|
+  with_scope(selector) do
+    click_link(link)
+  end
 end
 
-When /^(?:|I )follow "([^"]*)" within "([^"]*)"$/ do |link, parent|
-  click_link_within(parent, link)
+When /^(?:|I )fill in "([^"]*)" with "([^"]*)"(?: within "([^"]*)")?$/ do |field, value, selector|
+  with_scope(selector) do
+    fill_in(field, :with => value)
+  end
 end
 
-When /^(?:|I )fill in "([^"]*)" with "([^"]*)"$/ do |field, value|
-  fill_in(field, :with => value)
-end
-
-When /^(?:|I )fill in "([^"]*)" for "([^"]*)"$/ do |value, field|
-  fill_in(field, :with => value)
+When /^(?:|I )fill in "([^"]*)" for "([^"]*)"(?: within "([^"]*)")?$/ do |value, field, selector|
+  with_scope(selector) do
+    fill_in(field, :with => value)
+  end
 end
 
 # Use this to fill in an entire form with data from a table. Example:
@@ -51,19 +59,24 @@ end
 # TODO: Add support for checkbox, select og option
 # based on naming conventions.
 #
-When /^(?:|I )fill in the following:$/ do |fields|
-  fields.rows_hash.each do |name, value|
-    When %{I fill in "#{name}" with "#{value}"}
+When /^(?:|I )fill in the following(?: within "([^"]*)")?:$/ do |selector, fields|
+  with_scope(selector) do
+    fields.rows_hash.each do |name, value|
+      When %{I fill in "#{name}" with "#{value}"}
+    end
   end
 end
 
-When /^(?:|I )select "([^"]*)" from "([^"]*)"$/ do |value, field|
-  select(value, :from => field)
+When /^(?:|I )select "([^"]*)" from "([^"]*)"(?: within "([^"]*)")?$/ do |value, field, selector|
+  with_scope(selector) do
+    select(value, :from => field)
+  end
 end
 
-When /^(?:|I )unselect "([^\"]*)" from "([^\"]*)"$/ do |value, field|
-  element = select_option(value, field).element.attributes["value"].value
-  select_option(value, field).send("select").value.delete(element)
+When /^(?:|I )check "([^"]*)"(?: within "([^"]*)")?$/ do |field, selector|
+  with_scope(selector) do
+    check(field)
+  end
 end
 
 # Use this step in conjunction with Rail's datetime_select helper. For example:
@@ -72,17 +85,10 @@ When /^(?:|I )select "([^"]*)" as the date and time$/ do |time|
   select_datetime(time)
 end
 
-# Use this step when using multiple datetime_select helpers on a page or
-# you want to specify which datetime to select. Given the following view:
-#   <%= f.label :preferred %><br />
-#   <%= f.datetime_select :preferred %>
-#   <%= f.label :alternative %><br />
-#   <%= f.datetime_select :alternative %>
-# The following steps would fill out the form:
-# When I select "November 23, 2004 11:20" as the "Preferred" date and time
-# And I select "November 25, 2004 10:30" as the "Alternative" date and time
-When /^(?:|I )select "([^"]*)" as the "([^"]*)" date and time$/ do |datetime, datetime_label|
-  select_datetime(datetime, :from => datetime_label)
+When /^(?:|I )uncheck "([^"]*)"(?: within "([^"]*)")?$/ do |field, selector|
+  with_scope(selector) do
+    uncheck(field)
+  end
 end
 
 # Use this step in conjunction with Rail's time_select helper. For example:
@@ -93,11 +99,10 @@ When /^(?:|I )select "([^"]*)" as the time$/ do |time|
   select_time(time)
 end
 
-# Use this step when using multiple time_select helpers on a page or you want to
-# specify the name of the time on the form.  For example:
-# When I select "7:30AM" as the "Gym" time
-When /^(?:|I )select "([^"]*)" as the "([^"]*)" time$/ do |time, time_label|
-  select_time(time, :from => time_label)
+When /^(?:|I )choose "([^"]*)"(?: within "([^"]*)")?$/ do |field, selector|
+  with_scope(selector) do
+    choose(field)
+  end
 end
 
 # Use this step in conjunction with Rail's date_select helper.  For example:
@@ -106,156 +111,107 @@ When /^(?:|I )select "([^"]*)" as the date$/ do |date|
   select_date(date)
 end
 
-# Use this step when using multiple date_select helpers on one page or
-# you want to specify the name of the date on the form. For example:
-# When I select "April 26, 1982" as the "Date of Birth" date
-When /^(?:|I )select "([^"]*)" as the "([^"]*)" date$/ do |date, date_label|
-  select_date(date, :from => date_label)
-end
-
-When /^(?:|I )check "([^"]*)"$/ do |field|
-  check(field)
-end
-
-When /^(?:|I )uncheck "([^"]*)"$/ do |field|
-  uncheck(field)
-end
-
-When /^(?:|I )choose "([^"]*)"$/ do |field|
-  choose(field)
-end
-
 # Adds support for validates_attachment_content_type. Without the mime-type getting
 # passed to attach_file() you will get a "Photo file is not one of the allowed file types."
-# error message 
-When /^(?:|I )attach the file "([^"]*)" to "([^"]*)"$/ do |path, field|
-  type = path.split(".")[1]
-
-  case type
-  when "jpg"
-    type = "image/jpg" 
-  when "jpeg"
-    type = "image/jpeg" 
-  when "png"
-    type = "image/png" 
-  when "gif"
-    type = "image/gif"
-  end
-  
-  attach_file(field, path, type)
-end
-
-Then /^(?:|I )should see "([^"]*)"$/ do |text|
-  if response.respond_to? :should
-    response.should contain(text)
-  else
-    assert_contain text
+# error message
+When /^(?:|I )attach the file "([^"]*)" to "([^"]*)"(?: within "([^"]*)")?$/ do |path, field, selector|
+  with_scope(selector) do
+    attach_file(field, path)
   end
 end
 
-Then /^(?:|I )should see "([^"]*)" within "([^"]*)"$/ do |text, selector|
-  within(selector) do |content|
-    if content.respond_to? :should
-      content.should contain(text)
+Then /^(?:|I )should see JSON:$/ do |expected_json|
+  require 'json'
+  expected = JSON.pretty_generate(JSON.parse(expected_json))
+  actual   = JSON.pretty_generate(JSON.parse(response.body))
+  expected.should == actual
+end
+
+Then /^(?:|I )should see "([^"]*)"(?: within "([^"]*)")?$/ do |text, selector|
+  with_scope(selector) do
+    if page.respond_to? :should
+      page.should have_content(text)
     else
-      hc = Webrat::Matchers::HasContent.new(text)
-      assert hc.matches?(content), hc.failure_message
+      assert page.has_content?(text)
     end
   end
 end
 
-Then /^(?:|I )should see \/([^\/]*)\/$/ do |regexp|
+Then /^(?:|I )should see \/([^\/]*)\/(?: within "([^"]*)")?$/ do |regexp, selector|
   regexp = Regexp.new(regexp)
-  if response.respond_to? :should
-    response.should contain(regexp)
-  else
-    assert_match(regexp, response_body)
-  end
-end
-
-Then /^(?:|I )should see \/([^\/]*)\/ within "([^"]*)"$/ do |regexp, selector|
-  within(selector) do |content|
-    regexp = Regexp.new(regexp)
-    if content.respond_to? :should
-      content.should contain(regexp)
+  with_scope(selector) do
+    if page.respond_to? :should
+      page.should have_xpath('//*', :text => regexp)
     else
-      assert_match(regexp, content)
+      assert page.has_xpath?('//*', :text => regexp)
     end
   end
 end
 
-Then /^(?:|I )should not see "([^"]*)"$/ do |text|
-  if response.respond_to? :should_not
-    response.should_not contain(text)
-  else
-    assert_not_contain(text)
-  end
-end
-
-Then /^(?:|I )should not see "([^"]*)" within "([^"]*)"$/ do |text, selector|
-  within(selector) do |content|
-    if content.respond_to? :should_not
-      content.should_not contain(text)
+Then /^(?:|I )should not see "([^"]*)"(?: within "([^"]*)")?$/ do |text, selector|
+  with_scope(selector) do
+    if page.respond_to? :should
+      page.should have_no_content(text)
     else
-      hc = Webrat::Matchers::HasContent.new(text)
-      assert !hc.matches?(content), hc.negative_failure_message
+      assert page.has_no_content?(text)
     end
   end
 end
 
-Then /^(?:|I )should not see \/([^\/]*)\/$/ do |regexp|
+Then /^(?:|I )should not see \/([^\/]*)\/(?: within "([^"]*)")?$/ do |regexp, selector|
   regexp = Regexp.new(regexp)
-  if response.respond_to? :should_not
-    response.should_not contain(regexp)
-  else
-    assert_not_contain(regexp)
-  end
-end
-
-Then /^(?:|I )should not see \/([^\/]*)\/ within "([^"]*)"$/ do |regexp, selector|
-  within(selector) do |content|
-    regexp = Regexp.new(regexp)
-    if content.respond_to? :should_not
-      content.should_not contain(regexp)
+  with_scope(selector) do
+    if page.respond_to? :should
+      page.should have_no_xpath('//*', :text => regexp)
     else
-      assert_no_match(regexp, content)
+      assert page.has_no_xpath?('//*', :text => regexp)
     end
   end
 end
 
-Then /^the "([^"]*)" field should contain "([^"]*)"$/ do |field, value|
-  field_value = field_labeled(field).value
-  if field_value.respond_to? :should
-    field_value.should =~ /#{value}/
-  else
-    assert_match(/#{value}/, field_value)
+Then /^the "([^"]*)" field(?: within "([^"]*)")? should contain "([^"]*)"$/ do |field, selector, value|
+  with_scope(selector) do
+    field = find_field(field)
+    field_value = (field.tag_name == 'textarea') ? field.text : field.value
+    if field_value.respond_to? :should
+      field_value.should =~ /#{value}/
+    else
+      assert_match(/#{value}/, field_value)
+    end
   end
 end
 
-Then /^the "([^"]*)" field should not contain "([^"]*)"$/ do |field, value|
-  field_value = field_labeled(field).value
-  if field_value.respond_to? :should_not
-    field_value.should_not =~ /#{value}/
-  else
-    assert_no_match(/#{value}/, field_value)
+Then /^the "([^"]*)" field(?: within "([^"]*)")? should not contain "([^"]*)"$/ do |field, selector, value|
+  with_scope(selector) do
+    field = find_field(field)
+    field_value = (field.tag_name == 'textarea') ? field.text : field.value
+    if field_value.respond_to? :should_not
+      field_value.should_not =~ /#{value}/
+    else
+      assert_no_match(/#{value}/, field_value)
+    end
   end
 end
 
-Then /^the "([^"]*)" checkbox should be checked$/ do |label|
-  field = field_labeled(label)
-  if field.respond_to? :should
-    field.should be_checked
-  else
-    assert field.checked?
+Then /^the "([^"]*)" checkbox(?: within "([^"]*)")? should be checked$/ do |label, selector|
+  with_scope(selector) do
+    field_checked = find_field(label)['checked']
+    if field_checked.respond_to? :should
+      field_checked.should be_true
+    else
+      assert field_checked
+    end
   end
 end
 
-Then /^the "([^"]*)" checkbox should not be checked$/ do |label|
-  field = field_labeled(label)
-  if field.respond_to? :should_not
-    field.should_not be_checked
-  else
-    assert !field.checked?
+Then /^the "([^"]*)" checkbox(?: within "([^"]*)")? should not be checked$/ do |label, selector|
+  with_scope(selector) do
+    field_checked = find_field(label)['checked']
+    if field_checked.respond_to? :should
+      field_checked.should be_false
+    else
+      assert !field_checked
+    end
   end
 end
 
@@ -272,7 +228,7 @@ Then /^(?:|I )should have the following query string:$/ do |expected_pairs|
   query = URI.parse(current_url).query
   actual_params = query ? CGI.parse(query) : {}
   expected_params = {}
-  expected_pairs.rows_hash.each_pair{|k,v| expected_params[k] = v.split(',')} 
+  expected_pairs.rows_hash.each_pair{|k,v| expected_params[k] = v.split(',')}
 
   if actual_params.respond_to? :should
     actual_params.should == expected_params
