a57f24eb1b1eeffa8bd1f7fe5b4aa16020c54fe7
Charles DuBose <cdubose@ubuntu.(none)>
2010-08-18 16:38:21 -0500

    The fix for finding hidden elements by text was made unnecessary by a new version of Selenium that doesn't throw an exception but instead just returns '' when you look for the text of a hidden element. Joy

--- features/support/override_capybara_searchable_for_xpath.rb
+++ features/support/override_capybara_searchable_for_xpath.rb
@@ -1,8 +1,7 @@
 # - CD
 # Fix for capybara regex bug that was causing the regex expression to be re-escaped multiple times until the system overloaded.
-# Fix for Capybara with Selenium bug that was throwing errors when it looked for text on hidden items.
 # The regex escape part can be removed when Capybara has moved beyond version 0.3.9 as it has been fixed in the codebase after that version
-# came out. As of now, the fix for the Selenium text error is not in the Capybara codebase. An "Issue" has been created for it
+# came out.
 
     module Capybara
         module Searchable
@@ -17,10 +16,6 @@
 
                 results = all_unfiltered(locator)
 
-                if options[:visible] or Capybara.ignore_hidden_elements
-                    results = results.select { |n| n.visible? }
-                end
-
                 if options[:text]
 
                     if options[:text].kind_of?(Regexp)
@@ -28,8 +23,12 @@
                     else
                         regexp = Regexp.escape(options[:text])
                     end
+                  
+                    results = results.select { |n| n.text.match(regexp) }
+                end
 
-                    results = results.select { |n| begin n.text.match(regexp) rescue false end }
+                if options[:visible] or Capybara.ignore_hidden_elements
+                    results = results.select { |n| n.visible? }
                 end
 
                 results
