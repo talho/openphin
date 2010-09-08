bd373a04787da361d1ae72143789430c5870fedf
Andrew Duhan <andrewduhan@gmail.com>
2010-09-08 11:36:10 -0500

    Hack to make searches use http post.

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -1,10 +1,40 @@
 (function($) {
-	
-	$( document ) . ready ( 
+
+	$( document ) . ready (
 	  function() {
+        $('div#search-results div.pagination').children('a').click(function(e){
+          uri = $(this).attr("href").split("?")[0];
+          uriparams = $(this).attr("href").split("?")[1];
+          params = deParam(uriparams);
+          myform = '<form method="post" action="' + uri +'">';
+          $.each(params, function(key, value) {
+            myform += '<input type="hidden" name="' + key + '" value="' + value + '">'
+          })
+          myform += '<input type="hidden" name="authenticity_token" value="' + $("meta#authenticity-token").attr('content') + '" >'
+          myform += '</form>'
+          myform = $(myform).appendTo('body');
+          myform.submit();
+
+          return false;
+        });
+
+
+        function deParam(q) {
+            var e,
+            urlParams = {},
+            d = function (s) { return decodeURIComponent(s.replace(/\+/g, " ")); },
+            r = /([^&=]+)=?([^&]*)/g;
+
+            while (e = r.exec(q)) {
+                urlParams[d(e[1])] = d(e[2]);
+            }
+            return urlParams;
+        };
+
+
 	    $( 'div.advance_search_user' ).before( '<p><a class="advance_search_user" href="#">Advance Search</a></p>' );
 	    $( 'div.advance_search_user' ).hide();
-	    $('a.advance_search_user').toggle ( 
+	    $('a.advance_search_user').toggle (
 	      function() {
 	        $(this.parentNode.nextSibling).slideDown('slow');
 			$('div.quick_search_user input').val('');
@@ -22,11 +52,11 @@
 	  }
 	)
 
-	$( document ) . ready ( 
+	$( document ) . ready (
 	  function() {
 	    $( 'p.search_user_explanation' ).before( '<p><a class="search_user_explanation" href="#">Willcard (*) Explanation</a></p>' );
 	    $( 'p.search_user_explanation' ).hide();
-	    $('a.search_user_explanation').toggle ( 
+	    $('a.search_user_explanation').toggle (
 	      function() {
 	        $(this.parentNode.nextSibling).slideDown('slow');
 	        $(this).html('Willcard (*) Explanation');
