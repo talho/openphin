fc0598b62f9e49a61f620afd1d0cbb384f583c69
Charles DuBose <cdubose@ubuntu.(none)>
2010-09-15 14:32:43 -0500

    creating an ext_alert_steps file to hold quick alert calls, including default sets and submission controls

---
+++ features/step_definitions/ext/ext_alert_steps.rb
@@ -0,0 +1,23 @@
+
+When /^I select the following alert audience:$/ do |table|
+  When %{delayed jobs are processed}
+  When %{I click breadCrumbItem "Audience"}
+  When %{I select the following in the audience panel:}, table
+end
+
+When /^I fill in the ext alert defaults$/ do
+  When %{I fill in the following:}, table(%{
+    | Title   | H1N1 SNS push packs to be delivered tomorrow |
+    | Message | For more details, keep on reading...         |})
+  And %{I select "" from ext combo "Jurisdiction"}
+  And %{I check "E-mail"}
+end
+
+When /^I send the alert$/ do
+  And %{I click breadCrumbItem "Preview"}
+  Then %{I should have the "Preview" breadcrumb selected}
+
+  When %{I press "Send Alert"}
+  Then %{the "Alert Detail" tab should be open}
+  And %{the "Send Alert" tab should not be open}
+end
