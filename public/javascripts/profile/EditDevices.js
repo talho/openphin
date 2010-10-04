3563dcdfd08b2600d244c905ef40d4735f46040a
cacornell <cacornell@gmail.com>
2010-10-04 16:16:31 -0500

    Added add_device() and remove_device().

--- public/javascripts/profile/EditDevices.js
+++ public/javascripts/profile/EditDevices.js
@@ -3,7 +3,7 @@ Ext.ns("Talho");
 Talho.EditDevices = Ext.extend(Talho.ProfileBase, {
   constructor: function(config){
     var store = new Ext.data.Store({
-      autoDestroy: true, // destroy the store if the grid is destroyed
+      autoDestroy: true,
       //url: 'plants.xml', // load remote data using HTTP
       listeners: {scope: this, 'add': {fn: function(){ this.getPanel().doLayout(); }, delay: 10}},
       reader: new Ext.data.XmlReader({
@@ -17,30 +17,30 @@ Talho.EditDevices = Ext.extend(Talho.ProfileBase, {
     });
 
     var template = new Ext.XTemplate(
-      '<h2>Devices</h2>',
       '<ul class="devices">',
       '<tpl for=".">',
         '<li class="device-item">',
           '<p><span class="title minor">{value}</span>&nbsp;&nbsp;&nbsp;{type}</p>',
+          //'<a class="destroy">Delete</a>',
         '</li>',
       '</tpl>',
       '</ul>'
     );
 
-    var ttt = this;
     var item_list = [
       {xtype: 'container', layout: 'form', labelAlign: 'top', defaults:{width:400}, items:[
-        {xtype: 'dataview', name: 'user[devices]',
-          store: store,
-          tpl: template,
-          autoHeight: true,
-          multiSelect: true,
+        {xtype: 'container', layout: 'hbox', items:[
+          {xtype: 'button', text: 'Add device', handler: this.add_device, scope: this, width:'auto'},
+          {xtype: 'button', text: 'Remove device', handler: this.remove_device, scope: this, width:'auto'}
+        ]},
+        {xtype: 'spacer', height: '10'},
+        {xtype: 'dataview', name: 'user[devices]', store: store, tpl: template, emptyText: 'No devices to display',
+          height: 250, autoHeight: false, autoScroll: true,
+          multiSelect: false, singleSelect: true, itemSelector: 'li.device-item', selectedClass: 'device-selected',
           //overClass: 'x-view-over',
-          itemSelector: 'li.device-item',
-          selectedClass: 'device-selected',
-          emptyText: 'No devices to display',
           //plugins: [new Ext.DataView.DragSelector()],
-        }
+        },
+        {xtype: 'spacer', height: '15'}
       ]}
     ];
     var url = config.url + ".json";
@@ -51,12 +51,41 @@ Talho.EditDevices = Ext.extend(Talho.ProfileBase, {
     // Override the setValue() method where necessary
     this.getPanel().find("name", "user[devices]")[0].setValue = function(val){
       var store = this.getStore();
-      var devices = jQuery.map(val, function(e,i){ var toks=e.split(":"); return new store.recordType({type: toks[0], value: toks[1]}); });
+      var devices = jQuery.map(val, function(e,i){ var toks=e.split(":"); return new store.recordType({type:toks[0],value:toks[1]}); });
       store.add(devices);
     };
 
     this.getPanel().doLayout();
   },
+
+  add_device: function(){
+    var win = new Ext.Window({
+      title: "Add Device",
+      layout: 'hbox', layoutConfig: {defaultMargins:'10',pack:'center'},
+      width: 450,
+      items: [
+        {xtype: 'textfield', name: 'dev[value]', maxLength: '46', allowBlank: false},
+        {xtype: 'combo', name: 'dev[type]', editable: false, value: 'E-mail', triggerAction: 'all',
+         store: ['E-mail','Phone','SMS','Fax','Blackberry PIN']}
+      ]
+    });
+    win.addButton({xtype: 'button', text: 'Add', handler: function(){ this.add_cb(win); }, scope: this, width:'auto'});
+    win.addButton({xtype: 'button', text: 'Cancel', handler: function(){ win.close(); }, scope: this, width:'auto'});
+    win.show();
+  },
+  add_cb: function(win){
+    var type = win.find("name", "dev[type]")[0].getValue();
+    var val = win.find("name", "dev[value]")[0].getValue();
+    var store = this.getPanel().find("name", "user[devices]")[0].getStore();
+    store.add(new store.recordType({type:type,value:val}));
+    win.close();
+  },
+  remove_device: function(){
+    var dv = this.getPanel().find("name", "user[devices]")[0];
+    var store = dv.getStore();
+    store.remove(dv.getSelectedRecords());
+  },
+
   save: function(){
     var store = this.getPanel().find("name", "user[devices]")[0].getStore();
     alert(store.getRange()[0].data.toSource());
