f46cc9a97a8835f0492bcf9528437a69f1cbb8df
Ethan Waldo <ewaldo@talho.org>
2011-01-09 18:37:40 -0600

    Changed dashboard to have new links and an additional news feed

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -8,7 +8,8 @@ class DashboardController < ApplicationController
 	  @articles = Article.recent
     feed_urls = [
       "http://www.nhc.noaa.gov/gtwo.xml",
-      "http://www.nhc.noaa.gov/nhc_at2.xml"
+      "http://www.nhc.noaa.gov/nhc_at2.xml",
+      "http://www.weather.gov/alerts-beta/tx.php?x=1"
      ]
     @entries = []
     begin   
