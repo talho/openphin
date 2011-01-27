1112276bedad5d1cdce8dd22873356d2b6272616
cacornell <cacornell@gmail.com>
2011-01-27 10:11:28 -0600

    Added Return key.

--- public/javascripts/profile/ProfileBase.js
+++ public/javascripts/profile/ProfileBase.js
@@ -24,7 +24,7 @@ Talho.ProfileBase = Ext.extend(function(){}, {
       layout: 'hbox', layoutConfig: {defaultMargins:'10',pack:'center'},
       cls: 'primary-panel',
       closable: true,
-      keys: { key: Ext.EventObject.ENTER, shift: false, fn: this.save, scope: this },
+      keys: { key: [Ext.EventObject.ENTER, Ext.EventObject.RETURN], shift: false, fn: this.save, scope: this },
       autoWidth: true,
       autoScroll: true,
       itemId: config.id,
