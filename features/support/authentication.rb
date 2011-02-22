b18bbd2ac09c50f8d2d2240e67cf3ee71227293b
Ethan Waldo <ewaldo@talho.org>
2011-02-22 14:41:04 -0600

    Getting timeout issues when running tests on hydra rig.  Hopefully this will fix it.

--- features/support/authentication.rb
+++ features/support/authentication.rb
@@ -4,6 +4,7 @@ module FeatureHelpers
     
     def login_as(user)
       visit sign_in_path
+      Then %Q{I should see "Email"}
       fill_in "Email", :with => user.email
       fill_in "Password", :with => "Password1"
       click_button "Sign in"
