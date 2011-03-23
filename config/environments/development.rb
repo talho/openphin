8229fae258d0cf3b948bcf89c3019fa6439d1028
Charles DuBose <cdubose@ubuntu.(none)>
2011-03-23 13:41:25 -0500

    Removing rack-bug and usage of it in order to enable reload_plugin config option

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,16 +18,6 @@ config.action_view.debug_rjs                         = true
 config.action_controller.perform_caching             = false
 config.reload_plugins                                = true
 
-file = RAILS_ROOT + "/config/rack-bug.yml"
-rackbug_config=HashWithIndifferentAccess.new
-if File.exists?(file)
-  rackbug_config.merge! YAML.load(IO.read(file))
-end
-config.middleware.use("Rack::Bug",
-                      :ip_masks => (rackbug_config["ip_masks"].blank? ? [IPAddr.new("127.0.0.1"), IPAddr.new("10.0.0.0/8"), IPAddr.new("172.16.0.0/12"), IPAddr.new("192.168.0.0/16")] : rackbug_config["ip_masks"].split(',').map {|ip| IPAddr.new(ip)}),
-                      :password => rackbug_config["password"] || "secret",
-                      :secret_key => rackbug_config["secret_key"] || "epT5uCIchlsHCeR9dloOeAPG66PtHd9K8l0q9avitiaA/KUrY7DE52hD4yWY+8z1")
-
 # Don't care if the mailer can't send
 config.action_mailer.raise_delivery_errors = false
 PHIN_PARTNER_OID="2.16.840.1.114222.4.3.2.2.3.770"
