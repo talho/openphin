73314924ced1ce6fed9565342a4c1bcb13816f6f
Ethan Waldo <ewaldo@talho.org>
2011-05-03 12:04:09 -0500

    Fixed failing tests related to changing HAN alerts to use alert responses rather than custom acknowlegment logic.

--- app/models/service/swn/email/alert.rb
+++ app/models/service/swn/email/alert.rb
@@ -39,7 +39,7 @@ class Service::SWN::Email::Alert < Service::SWN::Email::Base
 --MIME_boundary
 Content-Type: text/xml; charset=UTF-8
 Content-Transfer-Encoding: 8bit
-Content-ID: <#{alert.distribution_id}@#{Agency[:agency_domain]}>
+Content-ID: <#{alert.alert_identifier}@#{Agency[:agency_domain]}>
 
 #{body}
 --MIME_boundary
@@ -73,7 +73,7 @@ EOF
     xml.swn(:sendNotification, xmlns => "http://www.sendwordnow.com/notification") do
       xml.swn(:pSendNotificationInfo) do
         xml.swn(:SendNotificationInfo) do
-          xml.swn(:id, alert.distribution_id + "-EMAIL")
+          xml.swn(:id, alert.alert_identifier + "-EMAIL")
           xml.swn(:custSentTimestamp, Time.now.utc.iso8601(3))
           add_sender xml
           add_notification xml
