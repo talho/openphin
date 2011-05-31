dc221bd6ca7edad32b3979c3a6401f142809c7a5
Andrew Duhan <andrewduhan@gmail.com>
2011-05-31 10:51:49 -0500

    Fix for role-application migration - rename role 'Superadmin' to 'SuperAdmin'

--- db/migrate/20110414211250_add_application_to_roles.rb
+++ db/migrate/20110414211250_add_application_to_roles.rb
@@ -2,11 +2,19 @@ class AddApplicationToRoles < ActiveRecord::Migration
   def self.up
     add_column :roles, :application, :string
     Role.find(:all).each { | r | r.update_attribute(:application, r.name=='Rollcall' ? 'rollcall' : 'phin') }
+    begin  
+      Role.find_by_name_and_application('Superadmin', 'phin').update_attributes(:name => 'SuperAdmin')
+    rescue
+    end
     Role.find_or_create_by_name_and_application_and_user_role_and_approval_required('SysAdmin', 'system', false, true)
   end
 
   def self.down
     Role.find_by_name_and_application('SysAdmin', 'system').destroy
+    begin
+      Role.find_by_name_and_application('SuperAdmin', 'phin').update_attributes(:name => 'Superadmin')
+    rescue
+    end
     remove_column :roles, :application
   end
 end
