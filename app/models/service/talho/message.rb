ab92c664161250f780dcdb0f1c9a706566c53d60
Charles DuBose <cdubose@ubuntu.(none)>
2011-06-03 16:54:02 -0500

    Removing the 'test' stuff. It was unnecesary

--- app/models/service/talho/message.rb
+++ app/models/service/talho/message.rb
@@ -1,6 +1,4 @@
 class Service::TALHO::Message < Service::TALHO::Base
-  load_configuration_file RAILS_ROOT+"/config/talho_service.yml"
-  load_configuration_file RAILS_ROOT+"/config/email.yml"
   
   property :message
   
@@ -8,9 +6,6 @@ class Service::TALHO::Message < Service::TALHO::Base
 
   def deliver
     # Need to find the messages that are being sent via a supported device
-    raise "Service::TALHO::Message: Message property is blank" if @message.blank?
-    config = Service::TALHO::Message.configuration
-    initialize_fake_delivery(config) if config.fake_delivery?
     devices = []
     
     if message.Behavior && message.Behavior.Delivery && message.Behavior.Delivery.Providers
@@ -30,16 +25,4 @@ class Service::TALHO::Message < Service::TALHO::Base
     talho_msg.deliver
   end
   
-  private
-  
-  def initialize_fake_delivery(config) # :nodoc:
-    SUPPORTED_DEVICES[device].each do |device|
-      device.instance_eval do
-        define_method(:perform_delivery) do
-          Service::TALHO::Message.deliveries << OpenStruct.new(:message => message)
-          config.options[:default_response] ||= "200 OK"
-        end
-      end
-    end
-  end
 end
