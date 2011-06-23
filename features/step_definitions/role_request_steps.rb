d7dcd60f60c7d93ea4907ef1e7b9c6f8c1f872ae
Andrew Duhan <andrewduhan@gmail.com>
2011-06-23 11:42:06 -0500

    Fix for malicious role request step

--- features/step_definitions/role_request_steps.rb
+++ features/step_definitions/role_request_steps.rb
@@ -153,13 +153,14 @@ Given /^I maliciously request the role "([^"]*)" in "([^"]*)"$/ do |role, jur|
   current_user.role_memberships.each{|rm|
     rid = rm.role_id
     jid = rm.jurisdiction_id
-    rhashes.push({:id=>rm.id, :role_id=>rid, :jurisdiction_id=>jid,:rname=>Role.find(rid).name, :jname=>Jurisdiction.find(jid).name, :state=>'unchanged' })
+    rhashes.push({:id=>rm.id, :role_id=>rid, :jurisdiction_id=>jid,:rname=>Role.find(rid).name, :jname=>Jurisdiction.find(jid).name, :state=>'unchanged', :type=> 'role' })
   }
   role = Role.find_by_name(role)
   jur = Jurisdiction.find_by_name(jur)
   rhashes.push({:id=>-1, :role_id=>role.id, :jurisdiction_id=>jur.id, :rname=> role.name, :jname=> jur.name, :state=>"new"})
-  When "I maliciously put data to \"/users/#{current_user.id}/profile.json\"", table(%{
-    | user[rq] | #{ActiveSupport::JSON.encode(rhashes)} |
-  })
+  script = "xhr = new XMLHttpRequest(); " +
+          "xhr.open('PUT','/users/#{current_user.id}/profile.json');" +
+          "xhr.setRequestHeader('Content-Type','application/x-www-form-urlencoded');" +
+          "xhr.send( #{ "'user%5Brq%5D=" + CGI.escape(rhashes.to_json) + "'" });"
+  page.execute_script(script)
 end
-
