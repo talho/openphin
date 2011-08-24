ba2b8a0bec5db23b7f74c7058b3d8ad1ac9badd4
Charles DuBose <cdubose@ubuntu.(none)>
2011-08-24 16:35:12 -0500

    adding HOST global even though it points to the wrong address

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,6 +1,7 @@
 # Settings specified here will take precedence over those in config/environment.rb
 
 UPLOAD_BASE_URI = "http://localhost:3000"
+HOST = 'localhost:3000'
 
 # The test environment is used exclusively to run your application's
 # test suite.  You never need to work with it otherwise.  Remember that
