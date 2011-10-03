c382e8cfd2f808cafcf74ca06993a0413b522c8c
Eddie Gomez <eddie@talho.org>
2011-10-03 13:26:44 -0500

    Modifying how recipes are read-in by report_recipe_registation

--- config/initializers/report_recipe_registration.rb
+++ config/initializers/report_recipe_registration.rb
@@ -8,12 +8,12 @@ Report::Recipe.find_by_sql("select id, audience_id from report_recipes").each do
 end
 
 # register recipe class models
-report_path = File.dirname(__FILE__)
+report_path = File.join(Rails.root,"app","models","report")
 Dir.glob(File.join(report_path,"**","*_recipe.rb")).each do |m|
-  class_name = m.sub(report_path + File::SEPARATOR, '').sub(/\.rb$/, '').camelize
+  class_name = m[m.rindex("/")+1,m.length].sub(/\.rb$/,'').camelize
   if class_name.end_with? 'Recipe'
     begin
-      klass = "#{self.name.split('::').first}::#{class_name}"
+      klass  = "Report::#{class_name}"
       recipe = Report::Recipe.find_by_type(klass)
       if recipe.nil?
         klass.constantize.create
