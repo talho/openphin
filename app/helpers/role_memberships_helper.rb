bd65226bfa57d7fcf032cd1d0a4e5b23bcc7f393
Richard Boldway <richard@boldway.org>
2012-05-15 10:52:42 -0500

    Add a check for empty array and ignore

--- app/helpers/role_memberships_helper.rb
+++ app/helpers/role_memberships_helper.rb
@@ -1,7 +1,7 @@
 module RoleMembershipsHelper
   
   def to_rpt(element)
-    if element.kind_of?(Array)
+    if element.kind_of?(Array) && !element.empty?
       if element.first.kind_of?(Hash)
         element.map{|rm| "#{rm['role']} in #{rm['jurisdiction']}"}.sort[0..1].join("</br>")
       end
