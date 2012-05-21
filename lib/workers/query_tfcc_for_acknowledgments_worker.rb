5355ad64f50024e722c337355408dc5e826de434
cdubose <cdubose@marlin.(none)>
2012-05-21 14:58:43 -0500

    cleaning up background rb to work with our extension environment. Must short-circuit the typical packet worker setup, shamefully.

--- lib/workers/query_tfcc_for_acknowledgments_worker.rb
+++ lib/workers/query_tfcc_for_acknowledgments_worker.rb
@@ -1,6 +1,7 @@
 class QueryTfccForAcknowledgmentsWorker < BackgrounDRb::MetaWorker
 	set_worker_name :query_tfcc_for_acknowledgments_worker
-
+	set_no_auto_load(true)
+  
 	def create(args = nil)
 		# this method is called, when worker is loaded for the first time
 	end
