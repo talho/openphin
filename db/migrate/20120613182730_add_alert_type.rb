7d8e0e2c8cd00490d7b819bb05e8c47402c205e2
Richard Boldway <richard@boldway.org>
2012-07-03 11:08:13 -0500

    Add in the Alert table edit and place NULL condition on updates

--- db/migrate/20120613182730_add_alert_type.rb
+++ db/migrate/20120613182730_add_alert_type.rb
@@ -1,6 +1,10 @@
 class AddAlertType < ActiveRecord::Migration
   def up
-    unless AlertAttempt.column_names.include?('alert_type')      
+
+    unless Alert.column_names.include?('alert_type')
+      add_column(:alerts, :alert_type, :string)
+    end
+    unless AlertAttempt.column_names.include?('alert_type')
       add_column(:alert_attempts, :alert_type, :string)     
     end
     unless AlertAckLog.column_names.include?('alert_type')      
@@ -9,10 +13,11 @@ class AddAlertType < ActiveRecord::Migration
     unless AlertDeviceType.column_names.include?('alert_type')      
       add_column(:alert_device_types, :alert_type, :string)
     end
-    
-    execute "update alert_attempts set alert_type = A.alert_type from alerts A inner join alert_attempts AA on A.id = AA.alert_id"
-    execute "update alert_ack_logs set alert_type = A.alert_type from alerts A inner join alert_ack_logs AA on A.id = AA.alert_id"
-    execute "update alert_device_types set alert_type = A.alert_type from alerts A inner join alert_device_types AA on A.id = AA.alert_id"
+
+    execute "UPDATE alerts SET alert_type = 'Alert' WHERE alert_type IS NULL"
+    execute "UPDATE alert_attempts SET alert_type = a.alert_type FROM alerts a WHERE a.id = alert_id AND alert_attempts.alert_type IS NULL"
+    execute "UPDATE alert_ack_logs SET alert_type = a.alert_type FROM alerts a WHERE a.id = alert_id AND alert_ack_logs.alert_type IS NULL"
+    execute "UPDATE alert_device_types SET alert_type = a.alert_type FROM alerts a WHERE a.id = alert_id AND alert_device_types.alert_type IS NULL"
   end
 
   def down
