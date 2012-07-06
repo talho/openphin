817f391fad0964e0d3bde46b9b1d2f23b1796ac5
Richard Boldway <rboldway@talho.org>
2012-07-06 14:30:40 -0500

    Fixed the updating to include is NULL test.

--- db/migrate/20120613182730_add_alert_type.rb
+++ db/migrate/20120613182730_add_alert_type.rb
@@ -1,18 +1,23 @@
 class AddAlertType < ActiveRecord::Migration
   def up
-    unless AlertAttempt.column_names.include?('alert_type')      
-      add_column(:alert_attempts, :alert_type, :string)     
+
+    unless Alert.column_names.include?('alert_type')
+      add_column(:alerts, :alert_type, :string)
     end
-    unless AlertAckLog.column_names.include?('alert_type')      
+    unless AlertAttempt.column_names.include?('alert_type')
+      add_column(:alert_attempts, :alert_type, :string)
+    end
+    unless AlertAckLog.column_names.include?('alert_type')
       add_column(:alert_ack_logs, :alert_type, :string)
     end
-    unless AlertDeviceType.column_names.include?('alert_type')      
+    unless AlertDeviceType.column_names.include?('alert_type')
       add_column(:alert_device_types, :alert_type, :string)
     end
-    
-    execute "update alert_attempts set alert_type = A.alert_type from alerts A inner join alert_attempts AA on A.id = AA.alert_id"
-    execute "update alert_ack_logs set alert_type = A.alert_type from alerts A inner join alert_ack_logs AA on A.id = AA.alert_id"
-    execute "update alert_device_types set alert_type = A.alert_type from alerts A inner join alert_device_types AA on A.id = AA.alert_id"
+
+    execute "UPDATE alerts SET alert_type = 'Alert' WHERE alert_type IS NULL"
+    execute "UPDATE alert_attempts SET alert_type = a.alert_type FROM alerts a WHERE a.id = alert_id AND alert_attempts.alert_type IS NULL"
+    execute "UPDATE alert_ack_logs SET alert_type = a.alert_type FROM alerts a WHERE a.id = alert_id AND alert_ack_logs.alert_type IS NULL"
+    execute "UPDATE alert_device_types SET alert_type = a.alert_type FROM alerts a WHERE a.id = alert_id AND alert_device_types.alert_type IS NULL"
   end
 
   def down
