3a8c238e9836fe05953499521aa4382c2ed79401
cdubose <cdubose@marlin.(none)>
2012-07-17 09:29:16 -0500

    Updating vms to not throw error, hopefully

--- lib/deploy_jobs.rb
+++ lib/deploy_jobs.rb
@@ -35,7 +35,7 @@ end
 require 'thinking_sphinx/deploy/capistrano'
 namespace :sphinx do
   task :stop, :roles => [:app] do
-     run "cd #{current_path}; RAILS_ENV=#{rails_env} bundle exec rake thinking_sphinx:stop"
+     run "cd #{release_path}; RAILS_ENV=#{rails_env} bundle exec rake thinking_sphinx:stop"
   end
   
   task :symlink_sphinx_indexes, :roles => [:app] do
