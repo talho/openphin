0232a54b2a48ae4cc47b9055f51812cb6a808fec
colinphanna <github@colinphanna.com>
2012-07-20 16:12:14 -0500

    svg detection

--- app/assets/javascripts/utility.js
+++ app/assets/javascripts/utility.js
@@ -48,3 +48,9 @@ if(Ext)
         }
     });
 }
+
+Ext.namespace("Talho.Detection");
+
+Talho.Detection.SVG = function () {
+  return (document.implementation.hasFeature("http://www.w3.org/TR/SVG11/feature#BasicStructure", "1.1") || document.implementation.hasFeature("http://www.w3.org/TR/SVG11/feature#Shape", "1.0"))
+}
