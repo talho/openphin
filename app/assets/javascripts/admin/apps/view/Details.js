11e612b034c0b889b9a179ea3a481f736ba9a9fc
cdubose <cdubose@marlin.(none)>
2012-11-27 11:07:29 -0600

    Changing default app checkbox to use check event instead of change because chrome doesn't do the focus change thing well

--- app/assets/javascripts/admin/apps/view/Details.js
+++ app/assets/javascripts/admin/apps/view/Details.js
@@ -14,7 +14,7 @@ Talho.Admin.Apps.view.Details = Ext.extend(Talho.Admin.Apps.view.Helper, {
       {xtype: 'textfield', itemId: 'name', name: 'app[name]', fieldLabel: 'App Name', listeners: {scope: this, 'change': this.field_change}},
       {xtype: 'textfield', itemId: 'title', name: 'app[title]', fieldLabel: 'App Title', listeners: {scope: this, 'change': this.field_change}},
       {xtype: 'textfield', itemId: 'domains', name: 'app[domains]', fieldLabel: 'Domains (Comma Separated)', listeners: {scope: this, 'change': this.field_change}},
-      {xtype: 'checkbox', itemId: 'is_default', name: 'app[is_default]', boxLabel: 'Is the default app', listeners: {scope: this, 'change': this.field_change} },
+      {xtype: 'checkbox', itemId: 'is_default', name: 'app[is_default]', boxLabel: 'Is the default app', listeners: {scope: this, 'check': this.field_change} },
       {xtype: 'textfield', itemId: 'new_user_path', name: 'app[new_user_path]', fieldLabel: 'New User Path (when redefined)', listeners: {scope: this, 'change': this.field_change}},
       {xtype: 'textfield', itemId: 'help_email', name: 'app[help_email]', fieldLabel: 'Help Email', listeners: {scope: this, 'change': this.field_change}},
       {xtype: 'combo', itemId: 'root_jurisdiction_id', name: 'app[root_jurisdiction_id]', editable: true, forceSelection: true, valueNotFoundText: '', fieldLabel: 'Root Jurisdiction', 
