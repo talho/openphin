d83eeea1c762f557b379d216a48cb2d02bd17284
cdubose <cdubose@marlin.(none)>
2012-12-20 10:05:49 -0600

    adding postgres_ext to the gemfile (to pre-add postgres array support prior to rails 4.0). Adding migration to create report schedule script.

---
+++ db/migrate/20121219215232_create_report_schedule.rb
@@ -0,0 +1,11 @@
+class CreateReportSchedule < ActiveRecord::Migration
+  def change
+    create_table :report_schedules do |t|
+      t.string :report_type
+      t.integer :days_of_week, array: true, default: []
+      t.integer :user_id
+            
+      t.timestamps
+    end 
+  end
+end
